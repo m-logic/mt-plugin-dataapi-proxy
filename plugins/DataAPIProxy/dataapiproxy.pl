fd0cbeb700fb74c2bc9e8eafd3e1b140e8d323d6
Makoto Tajima <taz@m-logic.co.jp>
2026-07-31 15:37:21 +0900

    Added: proxy search bypassed the Data API disabled-site restriction Added: Data API permission filters were skipped when accessed via the proxy Rewrited the proxy initialization.

--- plugins/DataAPIProxy/dataapiproxy.pl
+++ plugins/DataAPIProxy/dataapiproxy.pl
@@ -15,15 +15,58 @@ dataapiproxy.cgi/v2/sites/1/entries?search=test
 =cut
 
 my $PLUGIN_NAME = 'DataAPIProxy';
-my $VERSION = '1.1';
-my $plugin = new MT::Plugin::DataAPIProxy({
-    name => $PLUGIN_NAME,
-    version => $VERSION,
-    author_name => 'M-Logic, Inc.',
-    author_link => 'http://m-logic.co.jp/',
-});
+my $VERSION     = '1.1';
+my $plugin      = new MT::Plugin::DataAPIProxy(
+    {
+        name        => $PLUGIN_NAME,
+        version     => $VERSION,
+        author_name => 'M-Logic, Inc.',
+        author_link => 'http://m-logic.co.jp/',
+    }
+);
 
-if (MT->version_number >= 7) { # required MT7
+our $DataAPICoreRegistered = 0;
+
+our %DataAPICoreAliases;
+
+my $saved_init_plugins;
+if ( MT->version_number >= 7 ) {
+
+    if ( MT->instance && eval { MT->instance->id } eq 'data_api' ) {
+        for ( my $i = 0 ; my @caller = caller($i) ; $i++ ) {
+            next
+              unless ( $caller[3] || '' ) eq 'MT::App::DataAPI::init_plugins';
+            $DataAPICoreRegistered = 1;
+            last;
+        }
+    }
+
+    require MT::App::DataAPI;
+    no warnings 'once';
+    no warnings 'redefine';
+    $saved_init_plugins             = \&MT::App::DataAPI::init_plugins;
+    *MT::App::DataAPI::init_plugins = sub {
+        my $app = shift;
+
+        my $is_data_api = $app->id eq 'data_api';
+
+        if ( $is_data_api && $DataAPICoreRegistered ) {
+            my $register = \&MT::_register_core_callbacks;
+            no warnings 'redefine';
+            local *MT::_register_core_callbacks = sub {
+                my ( $class, $table ) = @_;
+                my %keep = map { $_ => $table->{$_} }
+                  grep { !$DataAPICoreAliases{$_} } keys %$table;
+                return 1 unless %keep;
+                return $register->( $class, \%keep );
+            };
+            return &$saved_init_plugins( $app, @_ );
+        }
+
+        my $result = &$saved_init_plugins( $app, @_ );
+        $DataAPICoreRegistered = 1 if $is_data_api;
+        return $result;
+    };
     MT->add_plugin($plugin);
 }
 
@@ -34,29 +77,31 @@ sub init_registry {
     require MT::DataAPI::Format;
     require MT::DataAPI::Resource;
     require MT::Import;
-    $plugin->registry({
-        config_settings => {
-            DataAPIProxyScript => {
-                default => 'dataapiproxy.cgi',
+    $plugin->registry(
+        {
+            config_settings => {
+                DataAPIProxyScript => {
+                    default => 'dataapiproxy.cgi',
+                },
             },
-        },
-        applications => {
-            dataapiproxy => {
-                handler => 'MT::App::DataAPIProxy',
-                script => sub { MT->config->DataAPIProxyScript },
-                methods   => sub { MT->app->core_methods() },
-                endpoints => sub { MT->app->core_endpoints() },
-                resources => sub { MT::DataAPI::Resource->core_resources() },
-                formats   => sub { MT::DataAPI::Format->core_formats() },
-                default_format => 'json',
-                query_builder =>
-                    '$Core::MT::DataAPI::Endpoint::Common::query_builder',
-                # This is for search endpoint.
-                default        => sub { MT->app->core_parameters() },
-                import_formats => sub { MT::Import->core_import_formats() },
+            applications => {
+                dataapiproxy => {
+                    handler   => 'MT::App::DataAPIProxy',
+                    script    => sub { MT->config->DataAPIProxyScript },
+                    methods   => sub { MT->app->core_methods() },
+                    endpoints => sub { MT->app->core_endpoints() },
+                    resources =>
+                      sub { MT::DataAPI::Resource->core_resources() },
+                    formats => sub { MT::DataAPI::Format->core_formats() },
+                    default_format => 'json',
+                    query_builder  =>
+                      '$Core::MT::DataAPI::Endpoint::Common::query_builder',
+                    default        => sub { MT->app->core_parameters() },
+                    import_formats => sub { MT::Import->core_import_formats() },
+                },
             },
-        },
-    });
+        }
+    );
 }
 
 1;
--- plugins/DataAPIProxy/lib/MT/App/DataAPIProxy.pm
+++ plugins/DataAPIProxy/lib/MT/App/DataAPIProxy.pm
@@ -4,9 +4,89 @@ use strict;
 use base 'MT::App::DataAPI';
 
 use MT::App;
+use MT::DataAPI::Endpoint::DataAPIProxy::Search;
+use Scalar::Util qw( refaddr );
 use constant DEBUG => 0;
 
-sub id {'dataapiproxy'}
+sub id { 'dataapiproxy' }
+
+our %Endpoints;
+
+sub endpoints {
+    my ( $app, $version ) = @_;
+
+    return $app->SUPER::endpoints($version)
+      unless $app->model('blog')->has_column('allow_data_api');
+    $Endpoints{$version} ||= $app->_compile_proxy_endpoints($version);
+}
+
+sub _compile_proxy_endpoints {
+    my ( $app, $version ) = @_;
+
+    my $compiled = $app->SUPER::_compile_endpoints($version);
+    my %endpoint_copy;
+    for my $endpoint ( @{ $compiled->{list} } ) {
+        my $copy = $endpoint;
+        if ( $endpoint->{id} eq 'search' ) {
+            $copy = {%$endpoint};
+            delete $copy->{handler_ref};
+            $copy->{handler} =
+'$DataAPIProxy::MT::DataAPI::Endpoint::DataAPIProxy::Search::search';
+        }
+        $endpoint_copy{ refaddr($endpoint) } = $copy;
+    }
+
+    my $clone_tree;
+    $clone_tree = sub {
+        my ($node) = @_;
+        return $node unless ref($node) eq 'HASH';
+        my $address = refaddr($node);
+        return $endpoint_copy{$address}
+          if exists $endpoint_copy{$address};
+        return { map { $_ => $clone_tree->( $node->{$_} ) } keys %$node };
+    };
+
+    return {
+        hash => {
+            map {
+                my $endpoint = $compiled->{hash}{$_};
+                $_ => $endpoint_copy{ refaddr($endpoint) }
+            } keys %{ $compiled->{hash} }
+        },
+        tree => $clone_tree->( $compiled->{tree} ),
+        list =>
+          [ map { $endpoint_copy{ refaddr($_) } } @{ $compiled->{list} } ],
+    };
+}
+
+sub init_plugins {
+    my $app    = shift;
+    my $result = $app->SUPER::init_plugins(@_);
+
+    $MT::Plugin::DataAPIProxy::DataAPICoreRegistered = 1;
+    return $result;
+}
+
+sub _register_core_callbacks {
+    my ( $app, $table ) = @_;
+
+    $app->SUPER::_register_core_callbacks($table) or return;
+
+    return 1 if $MT::Plugin::DataAPIProxy::DataAPICoreRegistered;
+
+    my $prefix = $app->id . '_';
+    my %aliases;
+    for my $name ( keys %$table ) {
+        next unless $name =~ m/\A\Q$prefix\E/;
+        ( my $alias = $name ) =~ s/\A\Q$prefix\E/data_api_/;
+        $aliases{$alias} = $table->{$name};
+    }
+    return 1 unless %aliases;
+
+    $app->SUPER::_register_core_callbacks( \%aliases ) or return;
+    $MT::Plugin::DataAPIProxy::DataAPICoreAliases{$_} = 1 for keys %aliases;
+    return 1;
+}
 
 sub init {
     my $app = shift;
@@ -28,70 +108,91 @@ sub session_user {
 sub dataapi {
     my $app = shift;
 
-    my ($author) = MT::App::login($app);
+    my ($author)    = MT::App::login($app);
     my ($mtsession) = MT::App::session($app);
-    # ensure no session for DataAPI
     delete $app->{session};
     my $access_token;
     my $session;
     if (DEBUG) {
-        require MT::Util::Log; MT::Util::Log->init();
+        require MT::Util::Log;
+        MT::Util::Log->init();
     }
-    if ($author && $mtsession) {
+    if ( $author && $mtsession ) {
         if ( MT->version_number < 7 || $author->can_sign_in_data_api ) {
-            MT::Util::Log->info( 'DataAPIProxy: user:' . $author->name ) if DEBUG;
-            my $session_id = $mtsession->get('dataapiproxy_session');
+            MT::Util::Log->info( 'DataAPIProxy: user:' . $author->name )
+              if DEBUG;
+            my $session_id      = $mtsession->get('dataapiproxy_session');
             my $session_created = 0;
             if ($session_id) {
-                if (!MT::App::DataAPI::session_user( $app, $author, $session_id )) {
-                    MT::Util::Log->info( 'MT::App::DataAPI::session_user failed. session_id=' . $session_id) if DEBUG;
+                if (
+                    !MT::App::DataAPI::session_user(
+                        $app, $author, $session_id
+                    )
+                  )
+                {
+                    MT::Util::Log->info(
+                        'MT::App::DataAPI::session_user failed. session_id='
+                          . $session_id )
+                      if DEBUG;
                     $session_id = undef;
                 }
             }
-            if (!$session_id) {
-                MT::Util::Log->info( 'DataAPIProxy: no dataapiproxy_session. start_session') if DEBUG;
+            if ( !$session_id ) {
+                MT::Util::Log->info(
+                    'DataAPIProxy: no dataapiproxy_session. start_session')
+                  if DEBUG;
                 MT::App::DataAPI::start_session( $app, $author, 0 );
                 $session_created = 1;
                 if (DEBUG) {
-                    MT::Util::Log->info( 'MT::App::DataAPI::start_session failed') unless $app->{session};
+                    MT::Util::Log->info(
+                        'MT::App::DataAPI::start_session failed')
+                      unless $app->{session};
                 }
             }
             $session = $app->{session}
-                or return $app->error( 'Invalid login', 401 );
+              or return $app->error( 'Invalid login', 401 );
             $session_id = $session->id;
             if ($session_created) {
-                $mtsession->set('dataapiproxy_session', $session_id );
+                $mtsession->set( 'dataapiproxy_session', $session_id );
                 $mtsession->save;
             }
             if (DEBUG) {
                 if ($session_created) {
-                    MT::Util::Log->info( 'created dataapi session:' . $session_id );
+                    MT::Util::Log->info(
+                        'created dataapi session:' . $session_id );
                 }
                 else {
-                    MT::Util::Log->info( 'load dataapi session:' . $session_id );
+                    MT::Util::Log->info(
+                        'load dataapi session:' . $session_id );
                 }
             }
             my $access_token_created = 0;
-            $access_token = $app->model('accesstoken')->load({session_id => $session_id});
-            if (!$access_token) {
+            $access_token =
+              $app->model('accesstoken')->load( { session_id => $session_id } );
+            if ( !$access_token ) {
                 my $token_id = $app->make_magic_token;
                 $access_token = $app->model('accesstoken')->new;
                 $access_token->id($token_id);
             }
-            $access_token->set_values({
-                session_id => $session_id,
-                start => time,
-            });
+            $access_token->set_values(
+                {
+                    session_id => $session_id,
+                    start      => time,
+                }
+            );
             $access_token->save;
             if (DEBUG) {
                 if ($access_token_created) {
-                    MT::Util::Log->info( 'created accesstoken:' . $access_token->id );
+                    MT::Util::Log->info(
+                        'created accesstoken:' . $access_token->id );
                 }
                 else {
-                    MT::Util::Log->info( 're-use accesstoken:' . $access_token->id );
+                    MT::Util::Log->info(
+                        're-use accesstoken:' . $access_token->id );
                 }
             }
-            $ENV{HTTP_X_MT_AUTHORIZATION} = 'MTAuth accessToken=' . $access_token->id;
+            $ENV{HTTP_X_MT_AUTHORIZATION} =
+              'MTAuth accessToken=' . $access_token->id;
         }
         else {
             MT::Util::Log->info('DataAPIProxy: api access prohibited') if DEBUG;
@@ -103,10 +204,10 @@ sub dataapi {
 
     my $clientId = $app->param('clientId') || 'DataAPIProxy';
     $app->request( 'data_api_current_client_id', $clientId );
-    my $result = $app->api(@_);
+    my $result      = $app->api(@_);
     my $endpoint_id = ( $app->current_endpoint || {} )->{id} || '';
     MT::Util::Log->info( 'endpoint: ' . $endpoint_id ) if DEBUG;
-    MT::App::takedown($app);
+
     return $result;
 }
 
